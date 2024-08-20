6d6a97a5b5e43af0f62e49a8b6754ca96e54e92d
Turtlez200 <88110937+adikeshn@users.noreply.github.com>
2024-08-20 17:34:36 -0400

    Update FirebaseHandler.js

--- Database-Demo/FirebaseHandler.js
+++ Database-Demo/FirebaseHandler.js
@@ -1,21 +1,20 @@
-// Import the functions you need from the SDKs you need
+
+import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyChtOLjHDMIopWxh_wlBkkNVKjcdBrk1hg",
-  authDomain: "hackathon-demo-2f65e.firebaseapp.com",
-  projectId: "hackathon-demo-2f65e",
-  storageBucket: "hackathon-demo-2f65e.appspot.com",
-  messagingSenderId: "1078961883896",
-  appId: "1:1078961883896:web:9e8ef89eaec0087df1890d",
-  measurementId: "G-G7PMCQC4YB"
+  apiKey: "AIzaSyArPEq3UgwYBdAVD7lhkFNxJTmCKZLOoMo",
+  authDomain: "hackathon-demo-7cac2.firebaseapp.com",
+  projectId: "hackathon-demo-7cac2",
+  storageBucket: "hackathon-demo-7cac2.appspot.com",
+  messagingSenderId: "389495360291",
+  appId: "1:389495360291:web:c87ff3376975ebb342da96",
+  measurementId: "G-8ZZQN17YVF"
 };
 
 // Initialize Firebase
